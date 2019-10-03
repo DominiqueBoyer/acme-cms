d3c846a532233573cc60ea2d25d38e0df385d718
Dominique <dominiqueboyer94@yahoo.com>
2019-10-02 23:45:59 -0700

    stuff

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const { UUID, UUIDV4, STRING } = Sequelize
 
-const conn = new Sequelize('postgres://localhost/acme_cms');
+const conn = new Sequelize('postgres://localhost/acme_cms',{logging:false});
 
 const Page = conn.define('page', {
   id:{
@@ -11,59 +11,62 @@ const Page = conn.define('page', {
   },
   title:{
     type: STRING,
-    unique: true,
-    notNull: true,
-    notEmpty: true
+    unique: true
   }
 })
 
 const mapAndSave = (pages)=> Promise.all(pages.map( page => Page.create(page)));
 
 const syncAndSeed = async()=> {
-  await conn.sync({force: true});
-  const home = await Page.create({title: 'Home Page'});
-  let pages = [
-    { title: 'About', parentId: home.id },
-    { title: 'Contact', parentId: home.id }
-  ];
-  const [ about, contact ] = await mapAndSave(pages);
+  try {
+    await conn.sync({force: false});
+    const home = await Page.create({title: 'Home Page'});
+    let pages = [
+      { title: 'About', parentId: home.id },
+      { title: 'Contact', parentId: home.id }
+    ];
+    const [ about, contact ] = await mapAndSave(pages);
 
-  pages = [
-    { title: 'About our Team', parentId: about.id },
-    { title: 'About our History', parentId: about.id },
-    { title: 'Phone', parentId: contact.id },
-    { title: 'Fax', parentId: contact.id }
-  ];
-  const [ team, history, phone, fax ] = await mapAndSave(pages);
-
+    pages = [
+      { title: 'About our Team', parentId: about.id },
+      { title: 'About our History', parentId: about.id },
+      { title: 'Phone', parentId: contact.id },
+      { title: 'Fax', parentId: contact.id }
+    ];
+    const [ team, history, phone, fax ] = await mapAndSave(pages);
+  }
+  catch(err){
+    console.log(err);
+  }
 };
 
 Page.belongsTo(Page, { as: 'parent'});
 
-Page.findHomePage = async function(){
-  return await Page.findOne({where: {title: 'Home Page'}})
-};
+// Page.findHomePage = async function(){
+//   return await Page.findOne({where: {title: 'Home Page'}})
+// };
 
-Page.prototype.findChildren = async function(){
-  return await Page.findAll({where: {parentId: this.id}})
-};
+// Page.prototype.findChildren = async function(){
+//   return await Page.findAll({where: {parentId: this.id}})
+// };
 
-Page.prototype.hierarchy = async function(){
-  return await Page.findAll({where: { id: this.parentId}});
-}
+//still working on this method
+// Page.prototype.hierarchy = async function(){
+//   return await Page.findAll({where: { id: this.parentId}});
+// }
 
 syncAndSeed()
-  .then(async()=>{
-    const home = await Page.findHomePage();
-    // console.log('hello', home.title);
-    const homeChildren = await home.findChildren();
-    // console.log('home children', homeChildren.map( page => page.title)); //[About, Contact];
-    const fax = await Page.findOne({ where: { title: 'Fax'}});
-    // console.log('fax', fax.title);
-    let hier = await fax.hierarchy();
-    // console.log('hierarchy', hier.map( page => page.title)); //['Fax', 'Contact', 'Home']
+  // .then(async()=>{
+  //   // const home = await Page.findHomePage();
+  //   // // console.log('hello', home.title);
+  //   // const homeChildren = await home.findChildren();
+  //   // // console.log('home children', homeChildren.map( page => page.title)); //[About, Contact];
+  //   // const fax = await Page.findOne({ where: { title: 'Fax'}});
+  //   // // console.log('fax', fax.title);
+  //   // let hier = await fax.hierarchy();
+  //   // // console.log('hierarchy', hier.map( page => page.title)); //['Fax', 'Contact', 'Home']
 
-  })
+  // })
 
 module.exports = {
   syncAndSeed,
--- server.js
+++ server.js
@@ -2,34 +2,28 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const { Page } = db.models;
-const path = require('path');
 
 app.get('/api/pages', async (req, res, next )=>{
   Page.findAll()
     .then( page => res.send(page))
-    .catch(next)
+    .catch(next);
 });
 
 
-//the req.params.id is not working here
+app.get('/api/pages/:id/children', async (req, res, next )=>{
+  Page.findAll({ where: {parentId: req.params.id}})
+    .then( pages => res.send(pages))
+    .catch(next);
+});
 
 
-// app.get('/api/pages/:id', async (req, res, next )=>{
-//   Page.findPl({ where: {id: req.params.id}})
-//     .then( page => res.send(page))
-//     .catch(next);
-// });
-// app.get('/api/pages/:id/children', async (req, res, next )=>{
-//   Page.findAll({ where: {parentId: req.params.id}})
-//     .then( page => res.send(page))
-//     .catch(next);
-// });
-
-// app.get('/api/pages/:id/siblings', async (req, res, next )=>{
-//   Page.findAll({ where: { parentId: req.params.parentId}}).filter( page => page.id !== req.params.id)
-//     .then( page => res.send(page))
-//     .catch(next);
-// });
+app.get('/api/pages/:id/siblings', async (req, res, next )=>{
+  console.log(req.params)
+  Page.findByPk( req.params.id )
+    .then(page => Page.findAll({where: {parentId: page.parentId}}))
+    .then( pages => res.send(pages))
+    .catch(next);
+});
 
 db.syncAndSeed()
   .then (()=>{
